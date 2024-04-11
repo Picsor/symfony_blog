89bd5ef1e0aec552b625b42ec665521586e510e0
picsor <fs2picsor@gmail.com>
2024-04-11 10:43:24 +0200

    Update AuthenticationService.php

--- src/Service/AuthenticationService.php
+++ src/Service/AuthenticationService.php
@@ -1,10 +1,17 @@
 <?php
+namespace App\Service;
+use App\Entity\User;
 
 class AuthenticationService
 {
-    public function AuthenticateJwt($jwt)
+    public function AuthenticateJwt($jwt, User $user, $target_role)
     {
-        if($jwt == "REALLY_SECURED_TOKEN")
+        # Should check the JWT token against a secure token
+        # if the jwt token is valid return true:
+        #    - the token is valid,
+        #    - the token contains the right claims (e.g. user is admin)
+        if($jwt == "REALLY_SECURED_TOKEN" &&
+            $user->getRoles() == $target_role)
         {
             return true;
         }
