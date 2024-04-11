bdf90bbb81db00f86b84a9b510386d54f0e7fbb1
picsor <fs2picsor@gmail.com>
2024-04-11 10:50:22 +0200

    Update AuthenticationService.php

--- src/Service/AuthenticationService.php
+++ src/Service/AuthenticationService.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Service;
 use App\Entity\User;
+use Doctrine\ORM\EntityManager;
 
 class AuthenticationService
 {
-    public function AuthenticateJwt($jwt, User $user, $target_role)
+    public function AuthenticateJwt($jwt, EntityManager $em, $target_role)
     {
         # Should check the JWT token against a secure token
         # if the jwt token is valid return true:
         #    - the token is valid,
         #    - the token contains the right claims (e.g. user is admin)
+        $user = $em->getRepository(User::class)->findAll()[0];
+
         if($jwt == "REALLY_SECURED_TOKEN" &&
             $user->getRoles() == $target_role)
         {
